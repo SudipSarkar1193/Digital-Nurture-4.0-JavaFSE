3108dba8272323039bf7c16c3e5bb5e5215f0527
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-06-30 04:58:05 +0530

    Added Main.java

--- Week3_Spring Core and Maven/Exercise 4 - Creating and Configuring a Maven Project/src/main/java/com/sudip/Main.java
+++ Week3_Spring Core and Maven/Exercise 4 - Creating and Configuring a Maven Project/src/main/java/com/sudip/Main.java
@@ -1,17 +1,16 @@
 package com.sudip;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(context == null){
+            System.out.println("Spring Context Not Initialized!");
+        }else{
+            System.out.println("Spring Context Initialized!");
         }
+        context.close();
     }
 }
