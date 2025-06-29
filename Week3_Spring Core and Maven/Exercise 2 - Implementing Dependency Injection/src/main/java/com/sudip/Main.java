d67f57dbb734c0c4939f1b90ffc60390a6debd45
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-06-30 04:25:55 +0530

    Added applicationContext.xml | Main.java

--- Week3_Spring Core and Maven/Exercise 2 - Implementing Dependency Injection/src/main/java/com/sudip/Main.java
+++ Week3_Spring Core and Maven/Exercise 2 - Implementing Dependency Injection/src/main/java/com/sudip/Main.java
@@ -1,17 +1,19 @@
 package com.sudip;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.sudip.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Load Spring context from applicationContext.xml
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Get BookService bean
+        BookService bookService = context.getBean("bookService", BookService.class);
+
+        // Run the service
+        bookService.addBook("Sharadindu Omnibus");
+
     }
 }
