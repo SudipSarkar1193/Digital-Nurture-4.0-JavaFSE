6badcc7964275c7f314bdfb6655d53a52fdabbc7
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-09 17:57:18 +0530

    Started working on 'Exercise 03 - Hello World RESTful Web Service'

---
+++ Week4_Spring REST using Spring Boot 3/Exercise 03 - Hello World RESTful Web Service/src/test/java/com/cognizant/springlearn/HelloWorld_RESTfulWebService/HelloWorldResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn.HelloWorld_RESTfulWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldResTfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
