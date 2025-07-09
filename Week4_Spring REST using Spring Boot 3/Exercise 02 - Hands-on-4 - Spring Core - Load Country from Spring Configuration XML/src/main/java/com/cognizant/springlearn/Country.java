048a4f5212843c81bcd1ca3ed99a4db486f6280e
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-09 17:03:09 +0530

    Added Loggers to Country class

--- Week4_Spring REST using Spring Boot 3/Exercise 02 - Hands-on-4 - Spring Core - Load Country from Spring Configuration XML/src/main/java/com/cognizant/springlearn/Country.java
+++ Week4_Spring REST using Spring Boot 3/Exercise 02 - Hands-on-4 - Spring Core - Load Country from Spring Configuration XML/src/main/java/com/cognizant/springlearn/Country.java
@@ -9,19 +9,27 @@ public class Country {
     private String code;
     private String name;
 
+    public Country() {
+        LOGGER.debug("Inside Country Constructor.");
+    }
+
     public String getCode() {
+        LOGGER.debug("Inside getCode()");
         return code;
     }
 
     public void setCode(String code) {
+        LOGGER.debug("Inside setCode()");
         this.code = code;
     }
 
     public String getName() {
+        LOGGER.debug("Inside getName()");
         return name;
     }
 
     public void setName(String name) {
+        LOGGER.debug("Inside setName()");
         this.name = name;
     }
 
