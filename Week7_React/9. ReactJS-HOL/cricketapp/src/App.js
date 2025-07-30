71663bfcddb94794323efd3029c3c9a41c8a8587
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-07-30 17:01:06 +0530

    Modified App.jsx : Added toggle btn

--- Week7_React/9. ReactJS-HOL/cricketapp/src/App.js
+++ Week7_React/9. ReactJS-HOL/cricketapp/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import './App.css';
-import ListofPlayers from './ListofPlayers';
-import IndianPlayers from './IndianPlayers';
+import React, { useState } from "react";
+import "./App.css";
+import ListofPlayers from "./ListofPlayers";
+import IndianPlayers from "./IndianPlayers";
 
 function App() {
-  const flag = true; 
+  const [flag, setFlag] = useState(true);
 
-  if (flag) {
-    return (
+  const toggleFlag = () => {
+    setFlag(!flag);
+  };
+
+  const BussinessComponent = () =>
+    flag ? (
       <div>
         <h1>List of Players</h1>
-        <ListofPlayers />
+        <ListofPlayers className="player-list" />
       </div>
-    );
-  } else {
-    return (
+    ) : (
       <div>
         <IndianPlayers />
       </div>
     );
-  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Cricket App</h1>
+        <button onClick={toggleFlag} className="toggle-button">
+          {flag ? "Show Indian Players" : "Show List of Players"}
+        </button>
+      </header>
+      <BussinessComponent />
+    </div>
+  );
 }
 
 export default App;
