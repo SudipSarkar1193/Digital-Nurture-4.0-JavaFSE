f0ababf4764329c6db08cf3df0c8964272b75261
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-08-01 01:51:09 +0530

    Added UserPage component

---
+++ Week7_React/12. ReactJS-HOL/ticketbookingapp/src/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function UserPage() {
+  return (
+    <div>
+      <h1>Welcome Back!</h1>
+      <p>You can now book your tickets.</p>
+      <button>Book Ticket</button>
+    </div>
+  );
+}
+
+export default UserPage;
